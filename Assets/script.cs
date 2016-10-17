0dc288bdad9a47ec3ec99f2c8941752ea767af3d
Karrion <al187874@uji.es>
2016-10-17 13:57:12 +0200

    0.0.4
    Implementados a su manera los modos patrulla y alerta

--- Assets/script.cs
+++ Assets/script.cs
@@ -9,14 +9,19 @@ public class script : MonoBehaviour
     private int destPoint = 0;
     private NavMeshAgent agent;
     private float visibilityDistance = 10f;
-    private bool modoAlerta = false;
     public GameObject player;
     private int fieldOfViewDegrees = 90;
+    private LineRenderer line;
+    enum Mode { Alert, Patrol };
+    Mode mode = Mode.Patrol;
+    private float alertTime = 0.0f;
+    private Vector3 currentPatrol;
 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        line = GetComponent<LineRenderer>();
         agent.autoBraking = false;
         GotoNextPoint();
     }
@@ -32,36 +37,55 @@ public class script : MonoBehaviour
         destPoint = (destPoint + 1) % points.Length;
     }
 
-    bool CanSeePlayer()
+    Mode CanSeePlayer()
     {
         RaycastHit hit;
-        Vector3 rayDirection = player.transform.position - transform.position;
-        if (!modoAlerta)
-        {
-            if ((Vector3.Angle(rayDirection, transform.forward)) <= fieldOfViewDegrees * 0.5f)
+        Vector3 rayDirection = player.transform.position - transform.position; 
+           if ((Vector3.Angle(rayDirection, transform.forward)) <= fieldOfViewDegrees * 0.5f)
             {
                 if (Physics.Raycast(transform.position, rayDirection, out hit, visibilityDistance))
                 {
-                    Debug.DrawRay(transform.position, hit.transform.position);
-                    modoAlerta = true;
-                    Debug.Log("Te veo");
-                    return (hit.transform.CompareTag("Player"));
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        mode = Mode.Alert;
+                        Debug.Log("Te veo");
+                        currentPatrol = agent.destination;
+                        line.SetPosition(0, transform.position);
+                        line.SetPosition(1, hit.transform.position);
+                        return mode;
+                    }else{
+                        Debug.Log("Veo pared");
+                        mode = Mode.Patrol;
+                        agent.destination = currentPatrol;
+                        return mode;
+                    }
                 }
             }
-        }
-        return false;
+        return Mode.Patrol;
     }
 
 
     void Update()
     {
-        if (agent.remainingDistance < 0.5f && !modoAlerta)
-            GotoNextPoint();
-
-        if (CanSeePlayer())
+        if(agent.remainingDistance < 0.5f && mode == Mode.Patrol)
         {
-            agent.autoBraking = true;
+            GotoNextPoint();
+        }
+
+        CanSeePlayer();
+        
+        if (mode.Equals(Mode.Alert))
+        {
             agent.destination = player.transform.position;
+            alertTime += Time.deltaTime;
+            if (alertTime >= 10.0f)
+            {
+                mode = Mode.Patrol;
+            } else if(CanSeePlayer() == Mode.Alert)
+            {
+                alertTime = 0.0f;
+            }
+                
         }
 
     }
