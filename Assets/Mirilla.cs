4247c76085513615dd28c6803d0b3afe3ba0d3b0
mayoscuro <jesumayoral@hotmail.com>
2016-10-17 17:36:28 +0200

    mirilla

---
+++ Assets/Mirilla.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirilla : MonoBehaviour {
+    private SpriteRenderer mirillaRenderer;
+    // Use this for initialization
+    void Start()
+    {
+        mirillaRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = Input.mousePosition;
+        // pos.z = transform.position.z - Camera.main.transform.position.z;
+        pos.z = 2.6f;
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+    }
+}
+
