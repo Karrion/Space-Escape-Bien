b898126cc68e3bbe670f586686b3f4692cc56cf7
mayoscuro <jesumayoral@hotmail.com>
2016-10-27 16:35:51 +0200

    camaraEnemiga
    -Hay que cambiarla por una camara de verdad, pero ya esta implementada.

---
+++ Assets/Scripts/Camara/Camara.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour {
+    private bool modoAlerta = false;//Para darselo al gamemanager cuando se pueda y que ponga los soldados en modo alerta al ver al personaje.
+    public GameObject objetivo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.up);
+        ray.origin = transform.position;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                modoAlerta = true;
+                Debug.Log("Visto");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
