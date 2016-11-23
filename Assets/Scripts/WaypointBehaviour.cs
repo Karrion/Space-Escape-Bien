6e7e47e49a8effa0fd722dd1c5c84d1d4bcd8307
ushiroM <carlosgalibar@gmail.com>
2016-11-23 19:07:56 +0100

    Haciendo grafos para busquedas del guardia
    en proceso

---
+++ Assets/Scripts/WaypointBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaypointBehaviour : MonoBehaviour {
+    public float value;
+    public List<WaypointBehaviour> neighbours;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
