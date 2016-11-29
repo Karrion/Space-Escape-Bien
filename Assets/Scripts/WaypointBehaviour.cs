15590b03765a659a6f5292ff624cf655f488d7f9
Mafura <monzonismanuel@gmail.com>
2016-11-29 12:35:39 +0100

    Me cagon la puta Grafo, me tenés recontrapodrido
    Es hora de ASPLASTAR

--- Assets/Scripts/WaypointBehaviour.cs
+++ Assets/Scripts/WaypointBehaviour.cs
@@ -3,16 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class WaypointBehaviour : MonoBehaviour {
-    public float value;
+    
     public List<WaypointBehaviour> neighbours;
+    public List<float> values = new List<float>();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    // Use this for initialization
+    void Start () {
+        
+    }
+	void Awake()
+    {
+        int index = 0;
+        foreach(WaypointBehaviour neighbour in neighbours)
+        {
+            Vector3 direction = Vector3.zero;
+            Vector3 origin = transform.position;
+            Vector3 target = Vector3.zero;
+            target = neighbour.transform.position;
+            direction = target - origin;
+            float distance = direction.magnitude;
+            values.Add(distance);
+        }
+    }
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
 }
